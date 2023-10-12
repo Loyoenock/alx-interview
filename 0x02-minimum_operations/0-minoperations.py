c525f7b8782edea508dc2e1125e3810c338ebdaf
Loyoenock <loyoenock@gmail.com>
2023-10-12 00:13:38 -0700

    Refactor code for minOperations function: Improved comments for better readability

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
+"""Module for minOperations"""
+
 
 def minOperations(n):
     """
+    minOperations
     Calculate the minimum number of operations required to obtain 'n' 'H' characters.
     """
     # All outputs should be at least 2 characters: (min, Copy All => Paste)
     if n < 2:
         return 0
 
-    operations, divisor = 0, 2
-
-    while divisor <= n:
-        # If 'n' is evenly divisible by 'divisor'
-        if n % divisor == 0:
-            # The total even divisions by 'divisor' contribute to the total operations
-            operations += divisor
+    ops, root = 0, 2
+    while root <= n:
+        # If 'n' is evenly divisible by 'root'
+        if n % root == 0:
+            # The total even divisions by 'root' contribute to the total operations
+            ops += root
             # Update 'n' to the remainder after division
-            n = n // divisor
-            # Decrease 'divisor' to find remaining smaller values that evenly divide 'n'
-            divisor -= 1
+            n = n // root
+            # Decrease 'root' to find remaining smaller values that evenly divide 'n'
+            root -= 1
+        # Increment 'root' until it evenly divides 'n'
+        root += 1
 
-        # Increment 'divisor' until it evenly divides 'n'
-        divisor += 1
-
-    return operations
+    return ops
